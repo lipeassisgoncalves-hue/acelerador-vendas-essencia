b934d6eb320e1abf2f8476e683fbc9a7ab9d0e60
lipeassisgoncalves-hue <lipe.assis.goncalves@gmail.com>
2026-07-07 17:21:12 -0300

    Delete src directory

--- src/utils/profile.js
+++
@@ -1,5 +0,0 @@
-import { profiles } from '../data/quiz'
-
-export function getProfile(totalScore) {
-  return Object.values(profiles).find(({ range }) => totalScore >= range[0] && totalScore <= range[1])
-}
